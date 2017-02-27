8066563cd3a4fbaef31533a0212d5b070e063e3b
rupin <rupin.chheda@gmail.com>
2017-02-27 09:32:54 +0530

    Week 1

---
+++ addTwoNumbers.py
@@ -0,0 +1,9 @@
+def addFunction(lA,lB):
+    return lA+lB
+
+print("Enter the First Number:")
+a=int(input())
+print("Enter the Second Number:")
+b=int(input())
+result=addFunction(a,b)
+print (result)
